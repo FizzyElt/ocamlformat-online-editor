93b2936fe8d772b21611a296383f5637158698bd
FizzyElt <fizzyelt.dev@outlook.com>
2025-03-21 23:09:35 +0800

    all select config

--- ocamlfmt_js/lib/config.ml
+++ ocamlfmt_js/lib/config.ml
@@ -1,6 +1,8 @@
 open Ocamlformat_lib
 open Js_of_ocaml
 
+let bool_values = [ "true", true; "false", false ]
+
 let make_option_updater name values updater =
   let insert (conf : Conf.t) value =
     let _, v = values |> List.find (fun (n, _) -> n = value) in
@@ -12,41 +14,291 @@ let make_option_updater name values updater =
 let config_options =
   let elt content = Conf.Elt.make content `Default in
   [ (* align_symbol_open_paren *)
+    make_option_updater "align_symbol_open_paren" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with align_symbol_open_paren = elt v } })
+  ; (* assignment_operator *)
     make_option_updater
-      "align_symbol_open_paren"
-      [ "true", true; "false", false ]
-      (fun conf v ->
-         { conf with fmt_opts = { conf.fmt_opts with align_symbol_open_paren = elt v } })
-    (* assignment_operator *)
-  ; make_option_updater
       "assignment_operator"
       [ "begin_line", `Begin_line; "end_line", `End_line ]
       (fun conf v ->
          { conf with fmt_opts = { conf.fmt_opts with assignment_operator = elt v } })
-    (* break_around_multiline_strings *)
-  ; make_option_updater
-      "break_around_multiline_strings"
-      [ "true", true; "false", false ]
+  ; (* break_around_multiline_strings *)
+    make_option_updater "break_around_multiline_strings" bool_values (fun conf v ->
+      { conf with
+        fmt_opts = { conf.fmt_opts with break_around_multiline_strings = elt v }
+      })
+  ; (* break_before_in *)
+    make_option_updater
+      "break_before_in"
+      [ "auto", `Auto; "fit_or_vertical", `Fit_or_vertical ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with break_before_in = elt v } })
+  ; (* break_cases *)
+    make_option_updater
+      "break_cases"
+      [ "fit", `Fit
+      ; "nested", `Nested
+      ; "toplevel", `Toplevel
+      ; "fit_or_vertical", `Fit_or_vertical
+      ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with break_cases = elt v } })
+  ; (* break_collection_expressions *)
+    make_option_updater
+      "break_collection_expressions"
+      [ "wrap", `Wrap; "fit_or_vertical", `Fit_or_vertical ]
       (fun conf v ->
          { conf with
-           fmt_opts = { conf.fmt_opts with break_around_multiline_strings = elt v }
+           fmt_opts = { conf.fmt_opts with break_collection_expressions = elt v }
          })
-  ; make_option_updater
+  ; (* break_colon *)
+    make_option_updater
+      "break_colon"
+      [ "before", `Before; "after", `After ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with break_colon = elt v } })
+  ; (* break_infix *)
+    make_option_updater
+      "break_infix"
+      [ "wrap", `Wrap
+      ; "fit_or_vertical", `Fit_or_vertical
+      ; "wrap_or_vertical", `Wrap_or_vertical
+      ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with break_infix = elt v } })
+  ; (* break_infix_before_func *)
+    make_option_updater "break_infix_before_func" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with break_infix_before_func = elt v } })
+  ; (* break_fun_decl *)
+    make_option_updater
+      "break_fun_decl"
+      [ "wrap", `Wrap; "fit_or_vertical", `Fit_or_vertical; "smart", `Smart ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with break_fun_decl = elt v } })
+  ; (* break_fun_sig *)
+    make_option_updater
+      "break_fun_sig"
+      [ "wrap", `Wrap; "fit_or_vertical", `Fit_or_vertical; "smart", `Smart ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with break_fun_sig = elt v } })
+  ; (* break_separators *)
+    make_option_updater
+      "break_separators"
+      [ "before", `Before; "after", `After ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with break_separators = elt v } })
+  ; (* break_sequences *)
+    make_option_updater "break_sequences" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with break_sequences = elt v } })
+  ; (* break_string_literals *)
+    make_option_updater
+      "break_string_literals"
+      [ "auto", `Auto; "never", `Never ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with break_string_literals = elt v } })
+  ; (* break_struct *)
+    make_option_updater "break_struct" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with break_struct = elt v } })
+  ; (* cases_matching_exp_indent *)
+    make_option_updater
+      "cases_matching_exp_indent"
+      [ "normal", `Normal; "compact", `Compact ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with cases_matching_exp_indent = elt v } })
+  ; (* disambiguate_non_breaking_match *)
+    make_option_updater "disambiguate_non_breaking_match" bool_values (fun conf v ->
+      { conf with
+        fmt_opts = { conf.fmt_opts with disambiguate_non_breaking_match = elt v }
+      })
+  ; (* doc_comments *)
+    make_option_updater
+      "doc_comments"
+      [ "before", `Before
+      ; "before_except_val", `Before_except_val
+      ; "after_when_possible", `After_when_possible
+      ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with doc_comments = elt v } })
+  ; (* doc_comments_tag_only *)
+    make_option_updater
+      "doc_comments_tag_only"
+      [ "fit", `Fit; "default", `Default ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with doc_comments_tag_only = elt v } })
+  ; (* dock_collection_brackets *)
+    make_option_updater "dock_collection_brackets" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with dock_collection_brackets = elt v } })
+  ; (* exp_grouping *)
+    make_option_updater
+      "exp_grouping"
+      [ "parens", `Parens; "preserve", `Preserve ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with exp_grouping = elt v } })
+  ; (* field_space *)
+    make_option_updater
+      "field_space"
+      [ "tight", `Tight; "loose", `Loose; "tight_decl", `Tight_decl ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with field_space = elt v } })
+  ; (* function_indent_nested *)
+    make_option_updater
+      "function_indent_nested"
+      [ "always", `Always; "auto", `Auto; "never", `Never ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with function_indent_nested = elt v } })
+  ; (* if_then_else *)
+    make_option_updater
+      "if_then_else"
+      [ "compact", `Compact
+      ; "fit_or_vertical", `Fit_or_vertical
+      ; "keyword_first", `Keyword_first
+      ; "k_r", `K_R
+      ; "vertical", `Vertical
+      ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with if_then_else = elt v } })
+  ; (* indicate_multiline_delimiters *)
+    make_option_updater
+      "indicate_multiline_delimiters"
+      [ "no", `No
+      ; "space", `Space
+      ; "closing_on_separate_line", `Closing_on_separate_line
+      ]
+      (fun conf v ->
+         { conf with
+           fmt_opts = { conf.fmt_opts with indicate_multiline_delimiters = elt v }
+         })
+  ; (* indicate_nested_or_patterns *)
+    make_option_updater
+      "indicate_nested_or_patterns"
+      [ "space", `Space; "unsafe_no", `Unsafe_no ]
+      (fun conf v ->
+         { conf with
+           fmt_opts = { conf.fmt_opts with indicate_nested_or_patterns = elt v }
+         })
+  ; (* infix_precedence *)
+    make_option_updater
+      "infix_precedence"
+      [ "indent", `Indent; "parens", `Parens ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with infix_precedence = elt v } })
+  ; (* leading_nested_match_parens *)
+    make_option_updater "leading_nested_match_parens" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with leading_nested_match_parens = elt v } })
+  ; (* let_and *)
+    make_option_updater
+      "let_and"
+      [ "compact", `Compact; "sparse", `Sparse ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with let_and = elt v } })
+  ; (* let_binding_deindent_fun *)
+    make_option_updater "let_binding_deindent_fun" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with let_binding_deindent_fun = elt v } })
+  ; (* let_binding_spacing *)
+    make_option_updater
+      "let_binding_spacing"
+      [ "compact", `Compact; "sparse", `Sparse; "double_semicolon", `Double_semicolon ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with let_binding_spacing = elt v } })
+  ; (* let_module *)
+    make_option_updater
+      "let_module"
+      [ "compact", `Compact; "sparse", `Sparse ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with let_module = elt v } })
+  ; (* line_endings *)
+    make_option_updater
+      "line_endings"
+      [ "lf", `Lf; "crlf", `Crlf ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with line_endings = elt v } })
+  ; (* match_indent_nested *)
+    make_option_updater
+      "match_indent_nested"
+      [ "always", `Always; "auto", `Auto; "never", `Never ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with match_indent_nested = elt v } })
+  ; (* module_item_spacing *)
+    make_option_updater
       "module_item_spacing"
       [ "compact", `Compact; "preserve", `Preserve; "sparse", `Sparse ]
       (fun conf v ->
          { conf with fmt_opts = { conf.fmt_opts with module_item_spacing = elt v } })
-  ; make_option_updater
-      "line_endings"
-      [ "lf", `Lf; "crlf", `Crlf ]
-      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with line_endings = elt v } })
-  ; make_option_updater
+  ; (* nested_match *)
+    make_option_updater
+      "nested_match"
+      [ "wrap", `Wrap; "align", `Align ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with nested_match = elt v } })
+  ; (* ocp_indent_compat *)
+    make_option_updater "ocp_indent_compat" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with ocp_indent_compat = elt v } })
+  ; (* parens_ite *)
+    make_option_updater "parens_ite" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with parens_ite = elt v } })
+  ; (* parens_tuple *)
+    make_option_updater
+      "parens_tuple"
+      [ "always", `Always; "multi_line_only", `Multi_line_only ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with parens_tuple = elt v } })
+  ; (* parens_tuple_patterns *)
+    make_option_updater
+      "parens_tuple_patterns"
+      [ "always", `Always; "multi_line_only", `Multi_line_only ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with parens_tuple_patterns = elt v } })
+  ; (* parse_docstrings *)
+    make_option_updater "parse_docstrings" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with parse_docstrings = elt v } })
+  ; (* sequence_blank_line *)
+    make_option_updater
       "sequence_blank_line"
       [ "compact", `Compact; "preserve_one", `Preserve_one ]
       (fun conf v ->
          { conf with fmt_opts = { conf.fmt_opts with sequence_blank_line = elt v } })
+  ; (* sequence_style *)
+    make_option_updater
+      "sequence_style"
+      [ "before", `Before; "separator", `Separator; "terminator", `Terminator ]
+      (fun conf v ->
+         { conf with fmt_opts = { conf.fmt_opts with sequence_style = elt v } })
+  ; (* single_case *)
+    make_option_updater
+      "single_case"
+      [ "compact", `Compact; "sparse", `Sparse ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with single_case = elt v } })
+  ; (* space_around_arrays *)
+    make_option_updater "space_around_arrays" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with space_around_arrays = elt v } })
+  ; (* space_around_lists *)
+    make_option_updater "space_around_lists" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with space_around_lists = elt v } })
+  ; (* space_around_records *)
+    make_option_updater "space_around_records" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with space_around_records = elt v } })
+  ; (* space_around_variants *)
+    make_option_updater "space_around_variants" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with space_around_variants = elt v } })
+  ; (* type_decl *)
+    make_option_updater
+      "type_decl"
+      [ "compact", `Compact; "sparse", `Sparse ]
+      (fun conf v -> { conf with fmt_opts = { conf.fmt_opts with type_decl = elt v } })
+  ; (* wrap_comments *)
+    make_option_updater "wrap_comments" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with wrap_comments = elt v } })
+  ; (* wrap_docstrings *)
+    make_option_updater "wrap_docstrings" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with wrap_docstrings = elt v } })
+  ; (* wrap_fun_args *)
+    make_option_updater "wrap_fun_args" bool_values (fun conf v ->
+      { conf with fmt_opts = { conf.fmt_opts with wrap_fun_args = elt v } })
   ]
 ;;
+
+(* int option *)
+(* 
+cases_exp_indent
+doc_comments_padding
+extension_indent
+function_indent
+indent_after_in
+let_binding_indent
+margin
+match_indent
+max_indent option
+stritem_extension_indent
+type_decl_indent
+*)
 
 type 'a matrix_js_t = 'a Js.js_array Js.t Js.js_array Js.t
 
