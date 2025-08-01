41856c8a8a1a523b4ef7afa1c62cfeb52b651c2c
FizzyElt <fizzyelt.dev@outlook.com>
2025-08-01 10:57:53 +0800

    improve function

--- ocamlfmt_js/lib/formatter.ml
+++ ocamlfmt_js/lib/formatter.ml
@@ -3,15 +3,12 @@ open Config
 open Js_of_ocaml
 
 let format conf content =
-  let new_conf = parse_config conf |> create_config in
-  let res =
-    Translation_unit.parse_and_format
-      Syntax.Use_file
-      ~input_name:"empty"
-      ~source:content
-      new_conf
-  in
-  match res with
-  | Ok content -> Js.some content
-  | _ -> Js.null
+  conf
+  |> parse_config
+  |> create_config
+  |> Translation_unit.parse_and_format
+       Syntax.Use_file
+       ~input_name:"empty"
+       ~source:content
+  |> Result.fold ~ok:Js.some ~error:(Fun.const Js.null)
 ;;
